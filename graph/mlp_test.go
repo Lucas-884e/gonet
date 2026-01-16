1bff5973876861e50c27c4f917534bca996c9bb9
lucas.lu <lucas.lu@bytedance.com>
2026-01-17 01:11:29 +0800

    minor changes

--- graph/mlp_test.go
+++ graph/mlp_test.go
@@ -7,7 +7,10 @@ import (
 )
 
 func TestMLP(t *testing.T) {
-	mlp := NewMLP(2)
+	var (
+		eps = 1e-10 // error should be less than this
+		mlp = NewMLP(2)
+	)
 
 	mlp.AddLayer(2, OpSigmoid)
 	mlp.LoadWeights([][][]float64{
@@ -17,9 +20,9 @@ func TestMLP(t *testing.T) {
 	out := mlp.Feed([]float64{-5, 2})
 	y1 := out[0]
 	y2 := out[1]
-	assert.Equal(t, 0.18242552380635635, y1.V())
+	assert.InDelta(t, 0.18242552380635635, y1.V(), eps)
 	assert.Equal(t, "σ(B_11+W_111×X_1+W_112×X_2)", y1.Name())
-	assert.Equal(t, 0.9890130573694068, y2.V())
+	assert.InDelta(t, 0.9890130573694068, y2.V(), eps)
 	assert.Equal(t, "σ(B_12+W_121×X_1+W_122×X_2)", y2.Name())
 
 	mlp.AddLayer(2, OpSoftmax)
@@ -31,6 +34,6 @@ func TestMLP(t *testing.T) {
 	out = mlp.Feed([]float64{-5, 2})
 	z1 := out[0]
 	z2 := out[1]
-	assert.Equal(t, 0.43471206139788876, z1.V())
-	assert.Equal(t, 0.5652879386021112, z2.V())
+	assert.InDelta(t, 0.43471206139788876, z1.V(), eps)
+	assert.InDelta(t, 0.5652879386021112, z2.V(), eps)
 }
