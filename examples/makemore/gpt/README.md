70b834042204265b881817c3ae0a5f4f297d9b7e
lucas.lu <lucas.lu@bytedance.com>
2026-04-30 01:04:16 +0800

    add gpt skeleton code with a two layered model

---
+++ examples/makemore/gpt/README.md
@@ -0,0 +1,7 @@
+# A Simple GPT (Decoder-Only Transformer)
+
+This example tries to train the same character-based GPT language model as in the
+[Let's build GPT: from scratch, in code, spelled out](https://www.youtube.com/watch?v=kCc8FmEb1nY&list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ&index=8)
+video of the "Neural Networks: Zero to Hero" series taught by Andrej Karpathy.
+
+## Model structure
---
+++ examples/makemore/gpt/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"math/rand/v2"
+	"os"
+	"time"
+
+	"github.com/Lucas-884e/gonet"
+	"github.com/Lucas-884e/gonet/util"
+)
+
+var (
+	data        = flag.String("i", "../dataset/shakespeare.txt", "Input data file path")
+	interactive = flag.Bool("interactive", false, "Turn on interactive mode")
+)
+
+const (
+	ctxLen   = 2 // context length
+	embDim   = 2 // embedding space dimension
+	headNum  = 2 // number of attention heads
+	layerNum = 2 // number of attention layers
+)
+
+func main() {
+	flag.Parse()
+
+	corpus := util.Must1(os.ReadFile(*data))
+	log.Printf("First 300 characters from corpus (size=%d): \n<|BEGIN|>\n%s\n<|END|>", len(corpus), corpus[:300])
+	c2i := util.GenVocabFromCorpus([][]byte{corpus}, '\n')
+	i2c := util.GetIndexToToken(c2i)
+	vocabSize := len(c2i)
+	log.Printf("Vocabulary (size=%d): %c", vocabSize, i2c)
+
+	inputs := util.TokensToIndexes(corpus, c2i)
+	log.Printf("Corpus encoded (first 50 indexes): %v", inputs[:50])
+
+	trainSize := len(inputs) * 9 / 10
+	trainSet, valSet := inputs[:trainSize], inputs[trainSize:]
+	log.Printf("Training set size=%d, validation set size=%d", trainSize, len(valSet))
+
+	model := gonet.SequentialModel(
+		gonet.EmbeddingLayer(gonet.NewEmbedding(vocabSize, embDim)),
+		gonet.DisembeddingLayer(gonet.NewEmbedding(vocabSize, embDim), true),
+	)
+	log.Printf("Generate:\n<|BEGIN|>\n%s\n<|END|>", generate(model, c2i, i2c, 100))
+
+	cfg := util.TrainConfig{
+		BatchSize:        50,
+		Epochs:           100,
+		LearningRate:     0.01,
+		LogEpochInterval: 20,
+	}
+	util.InteractiveTrain(&cfg, *interactive, func() time.Duration {
+		samples := randSamples(trainSet, ctxLen, 500)
+		return gonet.Train(model, samples, &cfg, gonet.CrossEntropyLoss)
+	})
+
+	log.Printf("Generate:\n<|BEGIN|>\n%s\n<|END|>", generate(model, c2i, i2c, 100))
+}
+
+func generate(model gonet.Model, c2i map[byte]int, i2c []byte, maxGenTokens int, ctx ...byte) []byte {
+	var ctxIdx []float64
+	if len(ctx) == 0 {
+		ctxIdx = []float64{0}
+	} else {
+		ctxIdx = util.NumberSliceConvert[int, float64](util.TokensToIndexes(ctx, c2i))
+	}
+
+	var genIdx []int
+	for range maxGenTokens {
+		if len(ctxIdx) > ctxLen {
+			ctxIdx = ctxIdx[len(ctxIdx)-ctxLen:]
+		}
+		var (
+			logits = model.Predict(ctxIdx)
+			probs  = util.Softmax(logits[len(logits)-len(i2c):])
+			idx    = util.RandMultinomial(probs)
+		)
+		genIdx = append(genIdx, idx)
+		ctxIdx = append(ctxIdx, float64(idx))
+	}
+
+	return util.IndexesToTokens(genIdx, i2c)
+}
+
+func randSamples(dataset []int, ctxLen, count int) (samples []util.Sample) {
+	for range count {
+		r := rand.IntN(len(dataset) - ctxLen)
+		samples = append(samples, util.Sample{
+			X: util.NumberSliceConvert[int, float64](dataset[r : r+ctxLen]),
+			Y: util.NumberSliceConvert[int, float64](dataset[r+1 : r+ctxLen+1]),
+		})
+	}
+	return
+}
+
+func sampleString(s util.Sample, i2c []byte) string {
+	var (
+		x = util.IndexesToTokens(util.NumberSliceConvert[float64, int](s.X), i2c)
+		y = util.IndexesToTokens(util.NumberSliceConvert[float64, int](s.Y), i2c)
+	)
+	return fmt.Sprintf("%+v | %q → %q", s, x, y)
+}
